3dde4d164522f9a4546ccfa351b4212801bc12fa
shamseer <mohammedshamseerp@gmail.com>
2022-01-15 23:34:53 -0800

    headerAndHerAdd

---
+++ js/App.js
@@ -0,0 +1,21 @@
+
+//Hero swiper sstart
+ var swiper = new Swiper(".mySwiper", {
+     speed:2000,
+    spaceBetween: 0,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    autoplay: {
+        delay: 1000,
+    },
+  });
+
+  //Hero swiper End
+
+
+
+ 
